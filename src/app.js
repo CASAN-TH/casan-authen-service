5c5bf6a2492622c325afe82327e13f661740ee5d
jigkoh3 <jigkoh3@hotmail.com>
2018-04-21 19:05:17 +0700

    fix issue auth with body-parser

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
+const bodyparser = require('body-parser');
 const app = express();
 
-module.exports = app;
+app.use(bodyparser.json());
 
 app.get('/api/auth/signin',(req,res)=>{
     res.json(req.body);
 });
 
+module.exports = app;
